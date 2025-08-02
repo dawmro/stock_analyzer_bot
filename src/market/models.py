6cd74e36c93582e93b1d7017103d2b00eaceee64
dawid mrotek <DawidMrotek@protonmail.ch>
2025-08-02 02:07:23 +0200

    [ADD] company model

--- src/market/models.py
+++ src/market/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=20, unique=True, db_index=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+
+    
+ 
