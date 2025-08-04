eb47e19f0172e85d89b203eee6ca135733140d47
dawid mrotek <DawidMrotek@protonmail.ch>
2025-08-04 05:35:49 +0200

    [UPDATE] django function as celery task

--- src/market/models.py
+++ src/market/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from timescale.db.models.fields import TimescaleDateTimeField
 from timescale.db.models.managers import TimescaleManager
+from . import tasks
+
 
 # Create your models here.
 class Company(models.Model):
@@ -14,6 +16,7 @@ class Company(models.Model):
     def save(self, *args, **kwargs):
         self.ticker = f"{self.ticker}".upper()
         super().save(*args, **kwargs)
+        tasks.sync_company_stock_quotes.delay(self.pk)
 
 
 class StockQuote(models.Model):
