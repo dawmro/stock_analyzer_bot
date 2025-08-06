eedd4de0fe497f80c0d84dfffb5063f540403a0a
dawid mrotek <DawidMrotek@protonmail.ch>
2025-08-06 04:19:21 +0200

    [FIX] missing days_ago value

--- src/market/tasks.py
+++ src/market/tasks.py
@@ -1,4 +1,5 @@
 import helpers.clients as helper_clients
+import math
 
 from celery import shared_task
 from django.apps import apps
@@ -11,6 +12,10 @@ from datetime import timedelta
 
 @shared_task
 def sync_company_stock_quotes(company_id, days_ago=32, date_format="%Y-%m-%d", verbose=True):
+    # 32 days is safe value because 1 datapoint per minute equals 1440 per day, is 46080 per 32 days, 
+    # which is less than the limit of 50000 datapoint per call from Polygon API, 
+    # yet it always covers at least entire month of data.
+ 
     Company = apps.get_model('market', 'Company')
     try:
         company_obj = Company.objects.get(id=company_id)
@@ -24,10 +29,11 @@ def sync_company_stock_quotes(company_id, days_ago=32, date_format="%Y-%m-%d", v
 
     multiplier = 1
     timespan = "minute"
-    end_date = timezone.now()
-    start_date = end_date - timedelta(days=days_ago)
+    now = timezone.now()
+    start_date = now - timedelta(days=days_ago)
     from_date = start_date.strftime(date_format)
-    to_date = end_date.strftime(date_format)
+    to_date = start_date + timedelta(days=days_ago + 1)
+    to_date = to_date.strftime(date_format)
 
     client = helper_clients.PolygonAPIClient(
         ticker=ticker, 
@@ -47,6 +53,7 @@ def sync_stock_data(days_ago = 2):
     Company = apps.get_model('market', 'Company')
     companies = Company.objects.filter(active=True).values_list('id', flat=True)
     for company_id in companies:
-        sync_company_stock_quotes.delay(company_id)
+        sync_company_stock_quotes.delay(company_id, days_ago=days_ago)
+
 
 
